1b3cf19c3457d8e7c2bafa3a33e5c059c2ee1292
Colleen Blake <colleenemilyblake@gmail.com>
2024-05-18 14:06:21 -0500

    starting ORM

--- MomAndPopShop/Models/Popcorn.cs
+++ MomAndPopShop/Models/Popcorn.cs
@@ -13,17 +13,21 @@ namespace MomAndPopShop.Models
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
         public string? Name { get; set; }
 
+
         [Required(ErrorMessage = "Description is required")]
         [StringLength(500, MinimumLength = 3, ErrorMessage = "Description must be between 3 and 500 characters")]
         public string? Description { get; set; }
+
 
         [Required(ErrorMessage = "Price is required")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be 0.01 or greater.")]
         [Precision(18, 2)]
         public decimal PopcornPrice { get; set; }
 
+
         [Range(0.01, int.MaxValue, ErrorMessage = "Quantity must be 1 or greater.")]
         public int? Quantity { get; set; }
+
 
         /*This is the SKU addition for stripe product connection*/
         public string? StripeSku { get; set; } = "";
--- MomAndPopShop/Models/Seasoning.cs
+++ MomAndPopShop/Models/Seasoning.cs
@@ -13,17 +13,22 @@ namespace MomAndPopShop.Models
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
         public string? Name { get; set; }
 
+
         [Required(ErrorMessage = "Description is required")]
         [StringLength(500, MinimumLength = 3, ErrorMessage = "Description must be between 3 and 500 characters")]
         public string? Description { get; set; }
+
 
         [Required(ErrorMessage = "Price is required")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be 0.01 or greater.")]
         [Precision(18, 2)]
         public decimal? SeasoningPrice { get; set; }
 
+
         [Range(0.01, int.MaxValue, ErrorMessage = "Quantity must be 1 or greater.")]
         public int? Quantity { get; set; }
+
+
         public Seasoning() { }
 
         public Seasoning(string name, string description, decimal? seasoningPrice)
